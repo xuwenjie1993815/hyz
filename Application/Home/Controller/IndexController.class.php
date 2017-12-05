804a4a061a14fd1e709f01513c14bdb069db066e
徐闻洁 <380935912@qq.com>
2017-12-05 15:09:22 +0800

    ..

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,9 +1,63 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-use LT\ThinkSDK\ThinkOauth;
 class IndexController extends Controller {
     public function index(){
         $this->display();
       }
+
+    //登录
+    //接收参数 username userpwd verify_code
+    public function checkLogin(){
+        $username = I('param.username');
+        $pass = I('param.userpwd');
+        $verify = I('param.verify_code');
+        // $error_url=U("login/index");
+        $error_url='';
+        $res = array('status'=>0);
+        //如果连续登录失败四次以上则验证验证码
+        $val = session('login_n');
+        $val++;
+        session('login_n', $val);
+        if($val >= 4){
+            if(!$this->check_verify($verify)){
+                if(IS_AJAX){
+                    $res['msg'] = '验证码错误';
+                    $this->ajaxReturn($res);
+                }else{
+                    $this->error('验证码错误', $error_url);
+                }
+            }
+        }
+
+        $data = D('User')->login($username,MD5($pass),$pass);
+        if($data['status']==1){
+            $url=U("Index/index");
+            if(!empty($_POST['jump_url'])){
+                $url=$_POST['jump_url'];
+            }
+
+            if(IS_AJAX){
+                $res['msg'] = '登录成功';
+                $this->ajaxReturn($res);
+            }else{
+                $this->success("登录成功",$url);
+            }
+        }else{
+            if(IS_AJAX){
+                $res['msg'] = $data["info"];
+                $this->ajaxReturn($res);
+            }else{
+                if($val == 3) $this->redirect('login/index');
+                else $this->error($data["info"], $error_url);
+            }
+        }
+    }
+
+    //验证验证码
+    public function check_verify($code, $id = ''){
+
+    }
+
+
 }
---
+++ Application/Home/Model/UserModel.class.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * 用户model
+ */
+namespace Think\Model;
+use Think\Model;
+class  userModel extends Model {
+    //独立用户model
+    //登录 注册 信息修改 修改密码
+
+	//登录
+	public function login($username,$pass,$pass_md){
+		// $where["user_name"] = array("eq",$username);
+		// $where['telephone'] = $username;
+
+		$where = "user_name = '$username' || tel = '$username'";
+
+		$result = M("user")->where($where)->find();
+		if (!$result) {
+			$data["status"] = 0;
+			$data["info"] = "用户名不存在";
+			return $data;
+		}
+
+		if ($result['salt']) {
+			if ($result["pass"] != md5($pass_md.$result['salt'])) {
+				$data["status"] = 0;
+				$data["info"] = "用户名或密码错误";
+				return $data;
+			}
+		}else{
+			if ($result["pass"] != $pass) {
+				$data["status"] = 0;
+				$data["info"] = "用户名或密码错误";
+				return $data;
+			}
+		}
+
+		if ($result["status"] != 1) {
+			$data["status"] = 0;
+			$data["info"] = "帐号已被锁定";
+			return $data;
+		}
+
+		session("user_id",$result["user_id"]);
+		session("user_name",$result["user_name"]);
+		session("real_name",$result["real_name"]);
+		$data["status"] = 1;
+		$data["info"] = "登录中";
+		$data["name"] = $result['real_name'];
+		$data["log_name"] = $username;
+		return $data;
+	}
+
+	//用户信息修改
+	public function edit($data){
+		//数据判断
+		$user_id = $data['user_id'];
+		$res = M("user")->where("user_id = '$user_id'")->save($data);
+		if ($res) {
+			return array('status' => 1,'msg' => '操作成功');
+		}
+		return array('status' => 0,'msg' => '操作失败');
+	}
+
+	//用户修改登录密码
+	function editPwd($data){
+		if($data){
+			if(!empty($data["newapass"])){
+				$salt = get_string();
+			    $data["pass"] = md5($data["newapass"].$salt);
+			    $data["salt"] = $salt;
+			}
+				if(empty($data["oldpass"])){
+					return array('status' => 0,'msg'=>'请输入原密码');
+			     }
+			     if(empty($data["newapass"])){
+			     	return array('status' => 0,'msg'=>'请输入新密码');
+			     }
+			     if($data["newapass"]!=$data["newapass2"]){
+			     	return array('status' => 0,'msg'=>'两次输入新密码不一致');
+			     }
+				$res = M("user")->where("user_id=".$_SESSION["user_id"])->find();
+				if (!$res['salt']) {
+					if($res['pass']!=md5($data["oldpass"])){
+					return array('status' => 0,'msg'=>'原密码错误');
+					}
+				}else{
+					if($res['pass']!=md5($data["oldpass"].$res['salt'])){
+					return array('status' => 0,'msg'=>'原密码错误');
+					}
+				}
+				if ($data['oldpass'] == $data['newapass']) {
+					return array('status' => 0,'msg'=>'新密码不能与近期用过密码相同');
+				}
+			$res = M("user")->where("user_id=".$_SESSION["user_id"])->save($data);
+			if($res!==false){
+				return array('status' => 1,'msg'=>'操作成功');
+			}else{
+				return array('status' => 0,'msg'=>'操作失败');
+			}
+		}else{
+			$adminmsg = M("user")->where("user_id=".$_SESSION["user_id"])->find();
+			$titles="我的信息";
+			return array('status' => 1,'data' => array('data' => $adminmsg,'msgtitle'=>$titles));
+		}
+	}
+
+}
