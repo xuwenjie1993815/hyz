890f070929a357c76a2085e8409b1975c286b50b
yanghui520 <156105667@qq.com>
2017-12-07 11:28:06 +0800

    活动列表和活动详情
    活动列表和活动详情接口

--- Application/Home/Controller/ActivityController.class.php
+++ Application/Home/Controller/ActivityController.class.php
@@ -1,16 +1,53 @@
 <?php
+namespace Home\Controller;
+use Think\Controller;
+use LT\ThinkSDK\ThinkOauth;
+class ActivityController extends Controller {
+	//活动列表
+	public function activityList()
+	{
+		$res = M('activity')->field('activity_id,images,title')->where(array('status'=>1))->order('ctime desc')->select();
+		if ($res) {
+			$data = array(
+                'status'=>0,
+                'msg'=>$res
+           	);
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        	$this->ajaxReturn($data);
+		}else{
+			$data = array(
+                'status'=>1,
+                'msg'=>'没有活动'
+           	);
 
-/**
- * Description of ActivityController
- *
- * @author Administrator
- */
-class ActivityController {
-    //put your code here
+        	$this->ajaxReturn($data);
+		}
+	}
+	//活动详情接口
+	public function activityDetail()
+	{
+		$activity_id = I('activity_id');
+		$activity_id = addslashes($activity_id);
+		$res = M('activity')->field('activity_id,images,activity_name,price,now_num,stop_time,activity_info')->where(array('activity_id'=>$activity_id))->find();
+		if ($res) {
+			$data = array(
+                'status'=>0,
+                'msg'=>$res
+           	);
+
+        	$this->ajaxReturn($data);
+		}else{
+			$data = array(
+                'status'=>1,
+                'msg'=>'activity_id无效'
+           	);
+
+        	$this->ajaxReturn($data);
+		}
+	}
+	//活动报名信息录入接口
+	public function enrollment()
+	{
+		
+	}
 }
