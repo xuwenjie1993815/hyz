0a7fb7769bae2ee01f2b3dca7eb1a38d2e53a3dc
yanghui520 <34259231+yanghui520@users.noreply.github.com>
2017-12-08 20:24:44 +0800

    1
    1

--- Application/Common/Model/SupportModel.class.php
+++ Application/Common/Model/SupportModel.class.php
@@ -4,6 +4,7 @@ namespace Common\Model;
 use Think\Model;
 
 class  SupportModel extends Model {
+	//生成唯一订单编号
     public function orderNumber()
     {
     	$order_number = date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
