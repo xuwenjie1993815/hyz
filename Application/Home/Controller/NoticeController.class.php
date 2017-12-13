cda4eeea8dfa5cb809cb510f6d4c6be86e13fe53
徐闻洁 <380935912@qq.com>
2017-12-13 11:25:52 +0800

    获取通知详情

--- Application/Home/Controller/NoticeController.class.php
+++ Application/Home/Controller/NoticeController.class.php
@@ -4,10 +4,10 @@ use Think\Controller;
 class NoticeController extends Controller
 {
     //通知列表
-    function notice_list()
+    public function notice_list()
     {
         //中奖表  通知表
-        $user_id = $_POST['user_id']?:3;
+        $user_id = $_POST['user_id'];
         $user_info = M('user')->where(array('user_id' => $user_id))->find();
         //确认用户登陆
         if (!$user_id) {
@@ -21,11 +21,53 @@ class NoticeController extends Controller
         $notice_list = M('notice')->where($where)->select();
         $ret['status'] = 0;
         $ret['msg'] = '获取成功';
-        $ret['notice_list'] = $notice_list;
+        $ret['data'] = $notice_list;
         $this->ajaxReturn($ret);
         die;
     }
 
     //获取通知详情
-
+    public function notice_info(){
+        $user_id = $_POST['user_id'];
+        $notice_id = $_POST['notice_id'];
+        $user_info = M('user')->where(array('user_id' => $user_id))->find();
+        //确认用户登陆
+        if (!$user_id) {
+            $ret['status'] = 1;
+            $ret['msg'] = '请先登陆';
+            $this->ajaxReturn($ret);
+            die;
+        }
+        if (!$notice_id){
+            $ret['status'] = 2;
+            $ret['msg'] = '缺少参数';
+            $this->ajaxReturn($ret);
+            die;
+        }
+        $where['notice_id'] = $notice_id;
+        $notice_info = M('notice')->where($where)->find();
+        if (!$notice_info){
+            $ret['status'] = 3;
+            $ret['msg'] = '获取失败';
+            $this->ajaxReturn($ret);
+            die;
+        }
+        if($notice_info['notice_status'] !== '1'){
+            $ret['status'] = 4;
+            $ret['msg'] = '通知已删除';
+            $this->ajaxReturn($ret);
+            die;
+        }
+        if($notice_info['user_id'] !== $user_id){
+            $ret['status'] = 5;
+            $ret['msg'] = '您无权查看';
+            $this->ajaxReturn($ret);
+            die;
+        }
+        $ret['status'] = 0;
+        $ret['msg'] = '获取成功';
+        $ret['data'] = $notice_info;
+        $this->ajaxReturn($ret);
+        die;
+    }
 }
