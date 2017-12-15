45e16293cb1dd9bd6fa67175e6648500b738d03e
xuwenjie1993815 <380935912@qq.com>
2017-12-15 21:38:44 +0800

    。。

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        $a = $_GET['aa'];
+        var_dump($a);
+        var_dump(json_decode($a,true));
         $this->display();
       }
 }
