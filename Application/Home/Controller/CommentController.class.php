039e7c48b900d221d4f4ba8603cd8ae731feea8c
徐闻洁 <380935912@qq.com>
2017-12-18 09:23:06 +0800

    ..

--- Application/Home/Controller/CommentController.class.php
+++ Application/Home/Controller/CommentController.class.php
@@ -34,7 +34,7 @@ class CommentController extends Controller{
     //获取活动评论子评论列表
     public function commentPList(){
         //用户名 用户头像 评论时间 评论内容 评论图片 评论点赞数 子评论数量
-        $pid = $_POST['pid']?:1;
+        $pid = $_POST['pid'];
         if (!$pid){
             $ret['status'] = 1;
             $ret['msg'] = '缺少参数';
