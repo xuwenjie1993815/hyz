01dcf87017e7736e84e5ded217b70bf2f3b12215
徐闻洁 <380935912@qq.com>
2017-12-29 11:51:14 +0800

    首页轮播

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,30 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->display();
+//        $this->display();
       }
+
+    //生成首页轮播
+    public function indexCarousel(){
+        $join = 'hyz_advert_list as l on l.aid = a.id';
+        $where['a.status'] = 1;
+        $order = 'a.id desc';
+        $advert_list = M('advert')->alias('a')->join($join)->where($where)->order($order)->select();
+        if ($advert_list){
+            $data = array(
+                'status'=>0,
+                'msg'=>'获取成功',
+                'data'=>$advert_list
+            );
+        }else{
+            $data = array(
+                'status'=>1,
+                'msg'=>'空数据',
+                'data'=>$advert_list
+            );
+        }
+        $this->ajaxReturn($data);
+    }
+
+
 }
